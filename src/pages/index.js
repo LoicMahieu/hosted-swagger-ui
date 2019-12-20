b12e575e3e8fce37f2bc0fb3d44e062c5748ee79
Loïc Mahieu <mahieuloic@gmail.com>
2019-12-20 08:57:35 +0100

    Add possibility to drop a file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,50 @@
-import React from "react";
+import React, { useCallback } from "react";
 import querystring from "querystring";
 import "swagger-ui-react/swagger-ui.css";
+import { useDropzone } from "react-dropzone";
+import { useState } from "react";
+
+const Drop = ({ onSrc }) => {
+  const onDrop = useCallback(acceptedFiles => {
+    const [file] = acceptedFiles;
+    const reader = new FileReader();
+
+    reader.onabort = () => console.log("file reading was aborted");
+    reader.onerror = () => console.log("file reading has failed");
+    reader.onload = () => {
+      const binaryStr = reader.result;
+      onSrc(binaryStr);
+    };
+    reader.readAsDataURL(file);
+
+    console.log(file);
+  }, [onSrc]);
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  return (
+    <div
+      {...getRootProps()}
+      style={{
+        position: "absolute",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        padding: 20
+      }}
+    >
+      <input {...getInputProps()} />
+      {isDragActive ? (
+        <pre>Drop the files here ...</pre>
+      ) : (
+        <pre>
+          Please provide a `url` query parameter or drag 'n' drop a swagger file
+          !
+        </pre>
+      )}
+    </div>
+  );
+};
 
 const DummyComponent = () => <div />;
 const SwaggerUI =
@@ -9,6 +53,7 @@ const SwaggerUI =
     : DummyComponent;
 
 const App = ({ location: { search } }) => {
+  const [src, setSrc] = useState();
   const query = search ? querystring.parse(search.replace(/^\?/, "")) : {};
   const { url } = query;
 
@@ -16,11 +61,13 @@ const App = ({ location: { search } }) => {
     return <pre>Loading...</pre>;
   }
 
-  if (!url) {
-    return <pre>Missing `url` parameter !</pre>;
+  if (src) {
+    return <SwaggerUI url={src} />;
+  } else if (url) {
+    return <SwaggerUI url={url} />;
   }
 
-  return <SwaggerUI url={url} />;
+  return <Drop onSrc={setSrc} />;
 };
 
 export default App;
